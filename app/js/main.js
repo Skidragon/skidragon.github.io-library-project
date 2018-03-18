a43e48c51ad099bb0943e445fb440515fb4a2bca
Simon <Simon_Freelance@hotmail.com>
2018-03-18 15:12:00 -0400

    Added a way to reset form for text and number inputs

--- app/js/main.js
+++ app/js/main.js
@@ -24,67 +24,74 @@ const addFormBtns = {
     submitBtn: document.querySelector('.js-btn-submit'),
     cancelBtn: document.querySelector('.js-btn-cancel'),
 }
+console.log(addForm.children);
 
 function resetForm() {
-
+    // If the element is an input and its a text or a number type then remove its value
+    for (let key in addFormInputs) {
+        const formInput = addFormInputs[key];
+        if (formInput.type === "text" || formInput.type === "number") {
+            formInput.value = "";
+        }
+    }
 }
 
-function addBookToLibrary() {
-    const grid = document.querySelector('.books__grid');
+    function addBookToLibrary() {
+        const grid = document.querySelector('.books__grid');
 
-    const tile = Tile.createTile();
+        const tile = Tile.createTile();
 
-    //Gets the book information from form and adds it to book tile
-    const titleInfo = addFormInputs.titleBox.value;
-    const authorInfo = addFormInputs.authorBox.value;
-    const pagesInfo = addFormInputs.pagesBox.value;
+        //Gets the book information from form and adds it to book tile
+        const titleInfo = addFormInputs.titleBox.value;
+        const authorInfo = addFormInputs.authorBox.value;
+        const pagesInfo = addFormInputs.pagesBox.value;
 
-    const book = new Book(titleInfo, authorInfo, pagesInfo, false);
-    const bookText = document.createElement('p');
-    const bookTextClasses = ['book-tile__info'];
-    bookText.classList.add(...bookTextClasses);
-    bookText.textContent = book.getInfo();
+        const book = new Book(titleInfo, authorInfo, pagesInfo, false);
+        const bookText = document.createElement('p');
+        const bookTextClasses = ['book-tile__info'];
+        bookText.classList.add(...bookTextClasses);
+        bookText.textContent = book.getInfo();
 
-    //Holds the buttons on the bottom of the tile
-    const btnContainer = document.createElement('div');
-    btnContainer.classList.add('book-tile__btn-container');
+        //Holds the buttons on the bottom of the tile
+        const btnContainer = document.createElement('div');
+        btnContainer.classList.add('book-tile__btn-container');
 
-    grid.appendChild(tile);
-    tile.appendChild(bookText);
-    tile.appendChild(btnContainer)
+        grid.appendChild(tile);
+        tile.appendChild(bookText);
+        tile.appendChild(btnContainer)
 
 
-    //createTrash argument deletes the target
-    const trashBtn = TileBtn.createTrash(tile);
+        //createTrash argument deletes the target
+        const trashBtn = TileBtn.createTrash(tile);
 
-    //Shows whether or not the book has been read
-    const readBtn = TileBtn.createRead(book.isRead);
+        //Shows whether or not the book has been read
+        const readBtn = TileBtn.createRead(book.isRead);
 
-    //This btn lets the user modify the text content in the tile
-    const editBtn = TileBtn.createEdit();
+        //This btn lets the user modify the text content in the tile
+        const editBtn = TileBtn.createEdit();
 
-    btnContainer.appendChild(trashBtn);
-    btnContainer.appendChild(readBtn);
-    btnContainer.appendChild(editBtn);
-    resetForm();
-}
+        btnContainer.appendChild(trashBtn);
+        btnContainer.appendChild(readBtn);
+        btnContainer.appendChild(editBtn);
+        resetForm();
+    }
 
-function init() {
-    addFormBtns.addBtn.addEventListener('click', function () {
-        addForm.style.display = 'flex';
-        body.style.overflow = 'hidden';
-    });
+    function init() {
+        addFormBtns.addBtn.addEventListener('click', function () {
+            addForm.style.display = 'flex';
+            body.style.overflow = 'hidden';
+        });
 
-    addFormBtns.submitBtn.addEventListener('click', function () {
-        addForm.style.display = 'none';
-        body.style.overflow = 'scroll';
-        addBookToLibrary()
-    });
+        addFormBtns.submitBtn.addEventListener('click', function () {
+            addForm.style.display = 'none';
+            body.style.overflow = 'scroll';
+            addBookToLibrary()
+        });
 
-    addFormBtns.cancelBtn.addEventListener('click', function () {
-        addForm.style.display = 'none';
-        body.style.overflow = 'scroll';
-    });
-}
+        addFormBtns.cancelBtn.addEventListener('click', function () {
+            addForm.style.display = 'none';
+            body.style.overflow = 'scroll';
+        });
+    }
 
-init();
+    init();
