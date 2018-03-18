8b8eb5e60c17bae561a83b5276f3d8cfe0bdda5a
Simon <Simon_Freelance@hotmail.com>
2018-03-18 15:35:03 -0400

    Setup & styled read btns to start coding functionality

--- app/js/form-components.js
+++ app/js/form-components.js
@@ -3,5 +3,6 @@ const addFormInputs = {
     titleBox: document.querySelector('#add-title-info'),
     authorBox: document.querySelector('#add-author-info'),
     pagesBox: document.querySelector('#add-pages-info'),
-    readBtns: document.querySelector('.js-read-me'),
+    yesBtn: document.querySelector('#js-read-me-yes'),
+    noBtn: document.querySelector('#js-read-me-no'),
 };
